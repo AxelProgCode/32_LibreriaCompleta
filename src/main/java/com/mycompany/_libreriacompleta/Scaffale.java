fd734684df1180d60ced4f58904c6121246178a9
Simone Plona <simoneplona2020@olivelliputelli.it>
2024-04-16 15:07:56 +0200

    refactoring dei case 7,8,9,10,11: problema con l'invocazione nella classe App e i messaggi da dare all'utente

--- src/main/java/com/mycompany/_libreriacompleta/Scaffale.java
+++ src/main/java/com/mycompany/_libreriacompleta/Scaffale.java
@@ -4,7 +4,7 @@
  */
 package com.mycompany._libreriacompleta;
 
-import java.io.Serializable;
+import java.io.*;
 import eccezioni.*;
 import utilita.*;
 
@@ -249,6 +249,177 @@ public class Scaffale implements Serializable
         elencoLibriOrdinati=Ordinatore.alfabeticoCrescente(elencoLibriOrdinati);
 	return elencoLibriOrdinati;
     }
+    
+    /**
+     * Esporta libri su file in formato CSV
+     * @param f1
+     * @param nomeFileCSV
+     * @param datiLibro
+     * @param s1
+     * @param lib
+     */
+    public void esportaCSV(TextFile f1, String nomeFileCSV, String datiLibro, Scaffale s1, Libro lib)
+    {
+        try
+        {
+            f1=new TextFile(nomeFileCSV, 'W');
+            for(int i=0;i<s1.getNumRipiani();i++)
+            {
+                for(int j=0;j<s1.getNumMaxLibri(i);j++)
+                {
+                    try
+                    {
+                        lib=s1.getLibro(i, j);
+                        datiLibro=i+";"+j+";"+lib.getTitolo()+";"+lib.getAutore()+";"+lib.getNumeroPagine();
+                        f1.toFile(datiLibro);
+                    }
+                    catch(FileException e)
+                    {
+                        System.out.println(e.toString());
+                    }
+                    catch(EccezioneRipianoNonValido | EccezionePosizioneNonValida e)
+                    {
+                        //non si verificherà mai
+                    }
+                    catch(EccezionePosizioneVuota e)
+                    {
+                        //non fare nulla, continua
+                    }
+                }
+            }
+            f1.closeFile();
+            System.out.println("Esportazione avvenuta correttamente!");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Errore: impossibile accedere al file!");
+        }
+    }
+    
+    /**
+     * Importa libri da file in formato CSV
+     * @param f1
+     * @param nomeFileCSV
+     * @param s1
+     * @param lib
+     */
+    public void importaCSV(TextFile f1, String nomeFileCSV, Scaffale s1, Libro lib)
+    {
+        try
+        {
+            f1=new TextFile(nomeFileCSV, 'R');
+            System.out.println("Caricamento...");
+            do{
+                try
+                {
+                    String rigaLetta=f1.fromFile();
+                    String[] datiVolume=rigaLetta.split(";");
+                    int ripiano=Integer.parseInt(datiVolume[0]);
+                    int posizione=Integer.parseInt(datiVolume[1]);
+                    String titolo=datiVolume[2];
+                    String autore=datiVolume[3];
+                    int numeroPagine=Integer.parseInt(datiVolume[4]);
+                    lib=new Libro(titolo, autore, numeroPagine);
+                    try
+                    {
+                        s1.setLibro(lib, ripiano, posizione);
+                        System.out.println("\tLibro "+titolo+" importato!");
+                    }
+                    catch(EccezioneRipianoNonValido e)
+                    {
+                        System.out.println("\tErrore: ripiano "+ripiano+" non valido per il libro "+titolo+"!");
+                    }
+                    catch(EccezionePosizioneNonValida e)
+                    {
+                        System.out.println("\tErrore: posizione "+posizione+" non valida per il libro "+titolo+"!");
+                    }
+                    catch(EccezionePosizioneOccupata e)
+                    {
+                        System.out.println("\tErrore: coordinata ("+ripiano+";"+posizione+") occupata! Libro "+titolo+" non importato!");
+                    }
+                }
+                catch(FileException e)
+                {
+                    System.out.println("Fine del file!");
+                    f1.closeFile();
+                    System.out.println("Importazione terminata!");
+                    break;
+                }
+            }while(true);
+        }
+        catch(IOException e)
+        {
+            System.out.println("Errore: impossibile accedere al file!");
+        }
+    }
+    
+    /**
+     * Salva dati su file binario
+     * @param s1
+     * @param nomeFileBIN
+     */
+    public void salvaDati(Scaffale s1, String nomeFileBIN)
+    {
+        try
+        {
+            ObjectOutputStream writer=new ObjectOutputStream(new FileOutputStream(nomeFileBIN));
+            writer.writeObject(s1);
+            writer.flush();
+            writer.close();
+            System.out.println("Salvataggio effettuato con successo!");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Errore: impossibile accedere al file!");
+        }
+    }
+    
+    /**
+     * Carica dati da file binario
+     * @param s1
+     * @param nomeFileBIN
+     */
+    public void caricaDati(Scaffale s1, String nomeFileBIN)
+    {
+        try
+        {
+            ObjectInputStream reader=new ObjectInputStream(new FileInputStream(nomeFileBIN));
+            s1=(Scaffale)reader.readObject();
+            reader.close();
+            System.out.println("Caricamento avvenuto con successo!");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Errore: impossibile accedere al file!");
+        }
+        catch(ClassNotFoundException e)
+        {
+            System.out.println("Errore: impossibile accedere al file!");
+        }
+    }
+    
+    /*/**
+     * Salva su file binario e chiude l'applicazione
+     * @param s1
+     * @param nomeFileBIN
+     */
+    /*public void salvaEsci(Scaffale s1, String nomeFileBIN)
+    {
+        try
+        {
+            ObjectOutputStream writer=new ObjectOutputStream(new FileOutputStream(nomeFileBIN));
+            writer.writeObject(s1);
+            writer.flush();
+            writer.close();
+            System.out.println("Salvataggio effettuato con successo!");
+        }
+        catch(IOException e)
+        {
+            System.out.println("Errore: impossibile accedere al file!");
+        }
+        voceMenuScelta=0;
+        System.out.println("Arrivederci!");
+    }*/
     
     /**
      * Restituisce una stringa con tutti i dati dei libri presenti nello scaffale
